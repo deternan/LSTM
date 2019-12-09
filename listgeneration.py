ca92e2edcc9196c7b509ff649c5c8479c6ca79d5
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-12-09 15:04:17 +0800

    listed the song list

---
+++ listgeneration.py
@@ -0,0 +1,48 @@
+# coding=utf8
+'''
+version: December 09, 2019 02:01 PM
+Last revision: December 09, 2019 02:40 PM
+
+Author : Chao-Hsuan Ke
+'''
+
+import os
+from os import listdir
+from os.path import isfile, isdir, join
+
+
+filePath = "D:\\Phelps\\GitHub\\Python\\LSTM\\data\\MayDay\\";        # folder name
+files = listdir(filePath)
+list = []
+
+# ouptut
+outputFolder = "D:\\Phelps\\GitHub\\Python\\LSTM\\data\\"                      # output folder name
+outputFile = "list.txt"
+fw = open(outputFolder + outputFile, "a")       # 'a' --> overlapping
+
+def readfolderfile(foldername):
+    folderfilepath = listdir(foldername)
+    for fileNames in folderfilepath:
+        #print(fileNames[0:len(fileNames)-4])
+        # print(fileNames)
+        list.append(fileNames[0:len(fileNames)-4])
+
+
+
+if (os.path.exists(filePath)):
+    for fileName in files:
+        fullpath = join(filePath, fileName)
+        if isfile(fullpath):
+            list.append(fileName[0:len(fileName)-4])
+            #print("file：", fileName)
+        elif isdir(fullpath):
+            #print("folder：", fileName)
+            readfolderfile(fullpath)
+
+for ss in list:
+    fw.write(ss+"\n")
+    print(ss)
+
+fw.close()
+
+print('files list: ',list)
